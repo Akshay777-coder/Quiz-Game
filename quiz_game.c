12bf6d29b864ba7ec35c401222bb1689c63c4085
Akshay Kumar <akshaykumar954012@gmail.com>
2025-12-01 01:17:02 +0530

    Update quiz_game.c
    removed typedef

--- quiz_game.c
+++ quiz_game.c
@@ -4,22 +4,22 @@
 #include <string.h>
 
 // Structure to hold question and answer
-typedef struct{
+struct Question{
     char question[200];
     char optionA[100];
     char optionB[100];
     char optionC[100];
     char optionD[100];
     char correctAnswer;
-}Question;
+};
 
 // Fisher-Yates shuffle algorithm
-void shuffleQuestions(Question questions[], int n) {
+void shuffleQuestions(struct Question questions[], int n) {
     for (int i = n - 1; i > 0; i--) {
         int j = rand() % (i + 1);
-        
+
         // Swap questions[i] and questions[j]
-        Question temp = questions[i];
+        struct Question temp = questions[i];
         questions[i] = questions[j];
         questions[j] = temp;
     }
@@ -27,7 +27,7 @@ void shuffleQuestions(Question questions[], int n) {
 
 int main() {
     // Initialize 50 questions from your screenshots
-    Question questionBank[50] = {
+    struct Question questionBank[50] = {
         {"Who developed the C programming language?", "Dennis Ritchie", "Bjarne Stroustrup", "James Gosling", "Ken Thompson", 'A'},
         {"What is the size of char data type in C?", "1 byte", "2 bytes", "4 bytes", "8 bytes", 'A'},
         {"Which header file is required for printf() function?", "stdlib.h", "stdio.h", "string.h", "math.h", 'B'},
@@ -38,7 +38,7 @@ int main() {
         {"What is the range of int data type (typically)?", "-32768 to 32767", "-2147483648 to 2147483647", "0 to 65535", "depends on system", 'B'},
         {"Which keyword is used to prevent variable modification?", "static", "const", "volatile", "extern", 'B'},
         {"What is the output of printf(%d, 5/2)?", "2", "2.5", "3", "error", 'A'},
-        
+
         {"What is the size of float in C?", "2 bytes", "4 bytes", "8 bytes", "16 bytes", 'B'},
         {"Which format specifier is used for unsigned int?", "%d", "%u", "%i", "%x", 'B'},
         {"What is the default value of static variables?", "0", "1", "garbage value", "undefined", 'A'},
@@ -49,7 +49,7 @@ int main() {
         {"Which is not a storage class in C?", "auto", "static", "volatile", "extern", 'C'},
         {"What is the ASCII value of 'A'?", "65", "97", "64", "96", 'A'},
         {"Which data type can store both positive and negative?", "unsigned int", "signed int", "unsigned char", "both A and B", 'B'},
-        
+
         {"What is the correct syntax for if statement?", "if condition then", "if (condition)", "if condition:", "if [condition]", 'B'},
         {"The 'continue' statement is used to:", "Exit function", "Skip iteration", "Stop program", "Pause program", 'B'},
         {"The escape sequence for new line is:", "\\t", "\\n", "\\t", "\\s", 'B' },
@@ -60,7 +60,7 @@ int main() {
         {"Which one is a storage class keyword?", "auto", "printf", "external", "scanf", 'A'},
         {"In C, 'a' represents:", "a digit", "an integer", "a character", "a word", 'C'},
         {"Number of arithmetic operators in C:", "4", "5", "6", "7", 'C'},
-        
+
         {"How many keywords are available in C?", "32", "34", "36", "40", 'A'},
         {"The keyword 'goto' is used for:", "Looping", "Jumping to a label", "Defining variables", "Ending program", 'B'},
         {"What does << operator represent?", "Logical OR", "Logical AND", "Bitwise left shift", "Bitwise right shift", 'C'},
@@ -70,7 +70,7 @@ int main() {
         {"Which is not a valid if-else statement?", "if ((char)x){}", "if (x) else {}", "if x else {}", "if (x){} else{}", 'C'},
         {"Which operator is used for logical NOT?", "!", "^", "&", "|", 'A'},
         {"Which is correct syntax for single-line comment?", "/* comment */", "// comment", "# comment", "* comment", 'B'},
-        
+
         {"Which data type holds decimal numbers?", "int", "double", "char", "long", 'B'},
         {"What will be output of: printf(%c, 'A'+1);?", "A", "B", "1", "ASCII value", 'B'},
         {"Which is a conditional control structure?", "switch", "goto", "for", "do-while", 'A'},
@@ -82,20 +82,20 @@ int main() {
         {"Which functions can be used for dynamic memory allocation?", "malloc()", "calloc()", "realloc()", "All of the above", 'D'},
         {"Which is correct way to declare pointer to an int?", "int *ptr;", "int ptr;", "int &ptr;", "pointer", 'A'}
     };
-    
+
     int score = 0;
     char userAnswer;
-    
+
     // Seed random number generator
     srand(time(NULL));
-    
+
     // Shuffle all 50 questions
     shuffleQuestions(questionBank, 50);
-    
+
     printf("\n\t\t***WELCOME_TO_THE_QUIZ_ARENA***\n");
     printf("\n\t*INSTRUCTION* You_Will_be_asked_ten_Random_Question");
     printf("\n\t\t  {Each_Correct_Answer = 1 mark}\n\n");
-    
+
     // Display first 10 questions one by one
     for (int i = 0; i < 10; i++) {
         printf("\nQuestion %d: %s", i + 1, questionBank[i].question);
@@ -105,12 +105,12 @@ int main() {
         printf("\nD.  %s", questionBank[i].optionD);
         printf("\nYour answer (A/B/C/D): ");
         scanf(" %s", &userAnswer);
-        
+
         // Convert to uppercase for comparison
         if (userAnswer >= 'a' && userAnswer <= 'd') {
             userAnswer = userAnswer - 32;
         }
-        
+
         // Check answer
         if (userAnswer == questionBank[i].correctAnswer) {
             printf("Correct! +1 mark \n");
@@ -118,15 +118,15 @@ int main() {
         } else {
             printf("Wrong! Correct answer was: %c \n", questionBank[i].correctAnswer);
         }
-        
-        
+
+
     }
-    
+
     // Display Final Score 
     printf("\n\t    ***QUIZ_COMPLETED!***\n");
     printf("\tYour final score: %d out of 10", score);
-    printf("\n\t     Percentage: %.2f%%\n\n",(score * 100.0) / 10);
-    
-    
+    printf("\n\t     Percentage: %.2f%%\n\n",(score * 10.0));
+
+
     return 0;
 }
